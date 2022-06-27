31d3b35365ed96cff36e22ca91654234f07c7129
Hagus Hermanto <80033875+HagusHermanto@users.noreply.github.com>
2022-06-27 11:34:36 +0700

    Add files via upload

---
+++ Segitiga_form_view.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>HASIL SETELAH TERJADINYA PERHITUNGAN</title>
+    </head>
+    <body>
+        <h2>Luas Segitiga</h2>
+        <form action="http://localhost:8080/Tugas/ci/index.php/segitiga" method="post">
+            Masukkan nilai alas:<br/>
+            <input type="text" name="alas1">
+            <br>Masukkan nilai tinggi:<br/>
+            <input type="text" name="tinggi1"> <br/>
+            <input type="submit" name="btnSubmit" value="Hitung">
+        </form>
+    </body>
+</html>
